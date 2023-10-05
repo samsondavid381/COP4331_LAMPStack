27bf2c46250673bcead91ad3f89dd7aaa841bd90
TerryDavisFan <145712564+TerryDavisFan@users.noreply.github.com>
2023-10-05 15:31:08 -0400

    Update loginController.php

--- api/v1/src/loginController.php
+++ api/v1/src/loginController.php
@@ -1,1 +1,20 @@
 <?php
+
+class loginController
+{
+    public function __construct(private loginGateway $gateway, private object $requestBody) {}
+
+    public function processRequest(string $method, string $username, string $password, object $requestBody) : void
+    {
+        if($method == "POST") {
+            $user = $this->gateway->getAccount($username, $password);
+            echo json_encode($user);
+        }
+        else 
+        {
+            http_response_code(400);
+            echo json_encode("The Request Verb is invalid");
+            exit;
+        }
+    }
+}   
