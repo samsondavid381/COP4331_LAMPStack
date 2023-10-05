d919d34f8234c7fe78228b4a59c39a919bdde836
TerryDavisFan <145712564+TerryDavisFan@users.noreply.github.com>
2023-10-05 15:35:09 -0400

    Create registerController.php

---
+++ api/v1/src/registerController.php
@@ -0,0 +1,28 @@
+<?php
+
+class registerController
+{
+    public function __construct(private registerGateway $gateway, private object $requestBody) {}
+
+    public function processRequest(string $method, string $username, string $password, string $confirm, object $requestBody) : void
+    {
+        if($password != $confirm)
+        {
+            http_response_code(400);
+            echo json_encode("Passwords do not match");
+            exit;
+        }
+        else {
+            if($method == "POST") {
+                $user = $this->gateway->addUser($username, $password);
+                echo json_encode($user);
+            }
+            else 
+            {
+                http_response_code(400);
+                echo json_encode("The Request Verb is invalid");
+                exit;
+            }
+        }
+    }
+}  
