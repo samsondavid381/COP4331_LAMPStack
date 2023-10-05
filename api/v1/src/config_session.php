62a0a9bd0b7a70e79e60330c8471e355bf6c14d3
TerryDavisFan <145712564+TerryDavisFan@users.noreply.github.com>
2023-10-05 13:42:54 -0400

    Update config_session.php

--- api/v1/src/config_session.php
+++ api/v1/src/config_session.php
@@ -5,7 +5,7 @@ ini_set("session.use_strict_mode", 1);
 
 session_set_cookie_params([
     'lifetime' => 1800,
-    'domain' => "",
+    'domain' => "localhost",
     'path' => "/",
     'secure' => true,
     'httponly' => true
