6bfcd085ed227accec87cef0ff9998f81320ec2d
TerryDavisFan <145712564+TerryDavisFan@users.noreply.github.com>
2023-10-05 13:53:02 -0400

    Update loginGateway.php

--- api/v1/src/loginGateway.php
+++ api/v1/src/loginGateway.php
@@ -2,23 +2,19 @@
 class loginGateway
 {
     private PDO $conn;
-    private string $username;
-    private string $password;
 
-    public function __construct(Database $database, string $username, string $password)
+    public function __construct(Database $database)
     {
         $this->conn = $database->getConnection();
-        $this->username = $username;
-        $this->password = $password;
     }
 
     //This function gets all info from database on a user based on their username and password
-    public function getAccount() {
+    public function getAccount(string $username, string $password) {
         $sql = "SELECT * FROM users WHERE Username = :username AND Password = :password;";
 
         $stmt = $this->conn->prepare($sql);
-        $stmt->bindValue(":username", $this->username);
-        $stmt->bindValue(":password", $this->password);
+        $stmt->bindValue(":username", $username);
+        $stmt->bindValue(":password", $password);
         $stmt->execute();
 
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
