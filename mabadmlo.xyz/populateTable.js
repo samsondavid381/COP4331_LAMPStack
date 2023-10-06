d8d5c878882d4e04f109678450b8016140e2e76a
samsondavid381 <136836294+samsondavid381@users.noreply.github.com>
2023-10-06 00:20:03 -0400

    Populate table edits

--- mabadmlo.xyz/populateTable.js
+++ mabadmlo.xyz/populateTable.js
@@ -13,24 +13,30 @@ function populateTableForUser(userId) {
 }
           
 function populateTable(contacts) {
-        const tableBody = document.getElementById('contactsTableBody');
-        contacts.forEach(contact => {
-            const row = tableBody.insertRow();
-            const firstNameCell = row.insertCell(0);
-            const lastNameCell = row.insertCell(1);
-            const phoneCell = row.insertCell(2);
-            const emailCell = row.insertCell(3);
-            const addressCell = row.insertCell(4);
+    const tableBody = document.getElementById('contactsTableBody');
+    contacts.forEach(contact => {
+        const row = tableBody.insertRow();
+        const firstNameCell = row.insertCell(0);
+        const lastNameCell = row.insertCell(1);
+        const phoneCell = row.insertCell(2);
+        const emailCell = row.insertCell(3);
           
-            firstNameCell.textContent = contact.first_name;
-            lastNameCell.textContent = contact.last_name;
-            phoneCell.textContent = contact.phone;
-            emailCell.textContent = contact.email;
-            addressCell.textContent = contact.address;
-            row.addEventListener('click', () => {
-                showUser(); 
-                showBlur(); 
-            });
+        firstNameCell.textContent = contact.FirstName;
+        lastNameCell.textContent = contact.LastName;
+        phoneCell.textContent = contact.PrimaryPhone;
+        emailCell.textContent = contact.PrimaryEmail;
+        row.addEventListener('click', () => {
+            showUser(contact); 
+            showBlur(); 
+        });
     });
+}
+function showUser(contact){
+    const userCard = document.getElementById("userCard");
+    userCard.style.display="block";
+    userCard.innerHTML = contact.FirstName + '<br>' + contact.LastName + '<br>' + contact.PrimaryPhone + '<br>' + contact.PrimaryEmail;
+}
+function hideUser(){
+    document.getElementById("userCard").style.display="none"
 }
           
