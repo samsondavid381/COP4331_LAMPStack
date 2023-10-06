a62eab2179a6704f4beb948de5c5bb1745a5154e
samsondavid381 <136836294+samsondavid381@users.noreply.github.com>
2023-10-05 22:03:26 -0400

    Login.js updated - test pls

--- mabadmlo.xyz/login.js
+++ mabadmlo.xyz/login.js
@@ -1,31 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const loginForm = document.getElementById("login-form");
-    const resultMessage = document.getElementById("resultMessage");
+function validateLoginForm() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var apiUrl = "https://api.mabadmlo.xyz/v1/login/" + encodeURIComponent(username) + "?password=" + encodeURIComponent(password);
 
-    loginForm.addEventListener("submit", function (event) {
-        event.preventDefault();
-        const username = document.getElementById("username").value;
-        const password = document.getElementById("password").value;
-        const apiUrl = "https://api.mabadmlo.xyz/v1/login/{username}?password={password}";
-        fetch(apiUrl, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ Username, Password}),
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                resultMessage.textContent = "Login successful!";
-                window.location.href = "home.html";
-            } else {
-                resultMessage.textContent = "Login failed. Please check your credentials.";
-            }
-        })
-        .catch(error => {
-            console.error("Error:", error);
-            resultMessage.textContent = "An error occurred. Please try again later.";
-        });
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    .then(response => {
+        if (response.ok) {
+            window.location.href = "home.html";
+        } else {
+            document.getElementById("resultMessage").innerHTML = "Username or password incorrect :( Try again";
+        }
+    })
+    .catch(error => {
+        console.error(error);
+        document.getElementById("resultMessage").innerHTML = "ERROR! :( ";
     });
-});
+}
