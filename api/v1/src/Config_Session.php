dc84ee60e773cdb52e482a9bc422dec856f8ab11
McElroy Ruman <ultraskull1000@gmail.com>
2023-10-08 03:14:26 -0400

    help

--- api/v1/src/Config_Session.php
+++ api/v1/src/Config_Session.php
@@ -5,7 +5,7 @@ ini_set("session.use_strict_mode", 1);
 
 session_set_cookie_params([
     'lifetime' => 0,
-    'domain' => "www.mabadmlo.xyz",
+    'domain' => "localhost",
     'path' => "/",
     'secure' => false,
     'httponly' => false
