f5ee1540fdf617372a21c8e08631d8c9e1f1e265
samsondavid381 <136836294+samsondavid381@users.noreply.github.com>
2023-10-08 18:01:17 -0400

    Update addcontact.js

--- mabadmlo.xyz/addcontact.js
+++ mabadmlo.xyz/addcontact.js
@@ -1,31 +1,27 @@
 
-    function add_contact(){
-        const firstName = document.getElementById('firstname').value;
-        const lastName = document.getElementById('lastname').value;
-        const phone = document.getElementById('phone').value;
-        const email = document.getElementById('email').value;
-        const userData = {
+function add_contact(){
+    const firstName = document.getElementById('firstname').value;
+    const lastName = document.getElementById('lastname').value;
+    const phone = document.getElementById('phone').value;
+    const email = document.getElementById('email').value;
+    const userData = {
             FirstName: firstName,
             LastName: lastName,
             PrimaryPhone: phone,
             PrimaryEmail: email
         };
-        const userId = getCookie('userId');
-        const apiUrl = `http://api.mabadmlo.xyz/v1/contacts/${userId}`;
+    const userId = getCookie('userId');
+    const apiUrl = `http://api.mabadmlo.xyz/v1/contacts/${userId}`;
 
-        fetch(apiUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(userData)
+    fetch(apiUrl, {
+    method: 'POST',
+    body: JSON.stringify(userData)
         })
         .then(response => response.json())
         .then(data => {
             console.log('Contact added successfully:', data);
         })
         .catch(error => {
-            // Handle errors here (e.g., show an error message)
             console.error('Error adding contact:', error);
         });
     }
