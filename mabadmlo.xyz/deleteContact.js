f28bb9a26693c1f7d22821e75f372ab53c010afa
lbterrell21 <145860251+lbterrell21@users.noreply.github.com>
2023-10-08 00:42:32 -0400

    Update deleteContact.js
    hopefully this works, this is so frustrating

--- mabadmlo.xyz/deleteContact.js
+++ mabadmlo.xyz/deleteContact.js
@@ -1,20 +1,21 @@
-function deleteRow(e, userId) {
-    apiURL='http://api.mabadmlo.xyz/v1/contacts/'+userId;
-    fetch(userId, {
+document.getElementById('cardDelButton').addEventListener('submit', function (e) {
+    const userId = '<?php echo $userID;?>';
+    const apiUrl = `http://api.mabadmlo.xyz/v1/contacts/${userId}`;
+
+    fetch(apiUrl, {
         method: 'DELETE',
     })
-    .then(response => {
-        if (response.ok) {
-            // Remove the row from the table upon successful deletion
-            const table = document.getElementById('contactsTableBody');
-            const deleteButton = e.target;
-            deleteButton.closest("tr").remove();
+    .then(response => response.json())
+    .then(data => {
+        const table = document.getElementById('contactsTableBody');
+        const deleteButton = e.target;
+        deleteButton.closest("tr").remove();
 
-            console.log('Data removed');
-        } else {
-            console.error('Error');
-        }
+        console.log('Contact deleted:', data);
     })
-    .catch(error => console.error('Error:', error));
-}
+    .catch(error => {
+        // Handle errors here (e.g., show an error message)
+        console.error('Error:', error);
+    });
 
+});
