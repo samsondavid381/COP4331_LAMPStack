3572fb26820d206af860352805d4c9cdc5e59161
samsondavid381 <136836294+samsondavid381@users.noreply.github.com>
2023-10-08 15:37:13 -0400

    Search function, sidebar

--- mabadmlo.xyz/search.js
+++ mabadmlo.xyz/search.js
@@ -1,15 +1,22 @@
 function searchContacts() {
-    var searchInput = document.getElementById("search-input").value.toLowerCase();
-    var table = document.querySelector('.table'); 
-    var rows = table.getElementsByTagName('tr'); 
+    let searchInput = document.getElementById("search-input").value.toLowerCase();
+    let table = document.querySelector('.table'); 
+    let rows = table.getElementsByTagName('tr'); 
 
-    for (var i = 1; i < rows.length; i++) {
-        var row = rows[i];
-        var firstNameCell = row.cells[0];
-        if (firstNameCell.textContent.toLowerCase().includes(searchInput)) {
-            row.style.display = ""; 
+    for (let i = 1; i < rows.length; i++) {
+        let row = rows[i];
+        let foundMatch = false;
+        for (let j = 0; j < row.cells.length; j++) {
+            let cell = row.cells[j];
+            if (cell.textContent.toLowerCase().includes(searchInput)) {
+                foundMatch = true;
+                break; 
+            }
+        }
+        if (foundMatch) {
+            row.style.display = "";
         } else {
-            row.style.display = "none";
+            row.style.display = "none"; 
         }
     }
 }
